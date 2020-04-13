197fe5f317c3763abdd7df4576df40856dce5078
autumn-ragland <raglanda998@gmail.com>
2020-04-13 13:59:20 -0500

    mongoose connection

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ let express = require('express');
 let app = express();
 let port = 8000;
 
+let mongoose = require('mongoose');
+let mongoDB = 'mongodb+srv://admin:C0d3Cr3w@cluster0-ueqkv.mongodb.net/cs_database_4?retryWrites=true&w=majority'
+console.log(`Connecting at ${mongoDB}`);
+mongoose.connect(mongoDB, {useNewUrlParser: true,  useUnifiedTopology: true });
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 let api = require('./routes/api');
 
 app.use('/api', api);
--- routes/api.js
+++ routes/api.js
@@ -2,28 +2,29 @@ let express = require('express');
 let router = express.Router();
 router.use(express.json());
 
-"mongodb+srv://admin:C0d3Cr3w@cluster0-ueqkv.mongodb.net/cs_database_4?retryWrites=true&w=majority"
+let ProductCollection = require('../models/ProductSchema');
 
-let productArray = [
-    {
-        'productID': 1,
-        'productName': 'chair',
-        'price': 25,
-        'quantity': 100,
-    },
-    {
-        'productID': 2,
-        'productName': 'desk',
-        'price': 100,
-        'quantity': 50,
-    },
-    {
-        'productID': 3,
-        'productName': 'lamp',
-        'price': 10,
-        'quantity': 2000,
-    }
-]
+// // test data
+// let productArray = [
+//     {
+//         'productID': 1,
+//         'productName': 'chair',
+//         'price': 25,
+//         'quantity': 100,
+//     },
+//     {
+//         'productID': 2,
+//         'productName': 'desk',
+//         'price': 100,
+//         'quantity': 50,
+//     },
+//     {
+//         'productID': 3,
+//         'productName': 'lamp',
+//         'price': 10,
+//         'quantity': 2000,
+//     }
+// ]
 
 // router.get('/test', (req,res) => {
 //     res.send("Test");
@@ -31,19 +32,19 @@ let productArray = [
 
 // get all products
 router.get('/', (req,res) => {
-    res.send(productArray);
+    // res.send(productArray);
 });
 
 // get product by ID
 router.get('/:productID', (req,res) => {
-    res.send(productArray[req.params.productID - 1]);
+    // res.send(productArray[req.params.productID - 1]);
 });
 
 // create a product
 router.post('/', (req,res) => {
-    req.body.productID = productArray.length + 1
-    res.send(req.body);
-    productArray.push(req.body);
+    // req.body.productID = productArray.length + 1
+    // res.send(req.body);
+    // productArray.push(req.body);
 })
 
 module.exports = router;
